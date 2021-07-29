e15d57c352ea525dee91c82deaa487c7fa3e6b50
CurlyFury <nuka@maill.com>
2021-07-29 17:30:47 +0800

    added todos logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 // import logo from './logo.svg';
 // import './App.css';
-
+import React from 'react';
 import TodoList from "./todo/TodoList";
+import Context from './context';
+import AddTodo from './todo/AddTodo';
+import { func } from 'prop-types';
 
 
 
 function App() {
 
-  const todos = [
-    { id: 1, completed: false, title: 'Buy some bread' },
-    { id: 2, completed: true, title: 'Buy some butter' },
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: 'Buy few eggs' },
+    { id: 2, completed: false, title: 'Buy some butter' },
     { id: 3, completed: false, title: 'Buy milk' },
-  ]
+  ])
+
+
+  function toggleTodo(id) {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title: title,
+      id: Date.now(),
+      completed: false,
+    }]))
+  }
 
   return (
-    <div className="wrapper">
-      <h1>Hello world ^^</h1>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>Hello world ^^</h1>
 
-      <TodoList todos={todos} />
-    </div>
+        {/* <AddTodo onCreate={addTodo} /> */}
+
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos :( </p>}
+
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/todo/TodoItem.js
+++ src/todo/TodoItem.js
@@ -1,5 +1,49 @@
-export default function TodoItem({ todo, index }) {
+
+import React, { useContext } from "react"
+
+import Context from "../context"
+
+const styles = {
+	li: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		padding: '.5rem 1rem',
+		border: '2px solid #ccc',
+		borderRadius: '5px',
+		margin: '5px'
+	},
+	span: {
+		margin: '5px'
+	},
+	input: {
+		marginRight: '1rem',
+		cursor: 'pointer'
+	}
+}
+
+
+function TodoItem({ todo, index, onChange }) {
+	const { removeTodo } = useContext(Context)
+	const classes = []
+
+	if (todo.completed) {
+		classes.push('isDone')
+	}
+
 	return (
-		<li><strong>{index + 1}</strong>{todo.title}</li>
+		<li style={styles.li}>
+			<span className={classes.join(' ')}>
+				<input
+					checked={todo.completed}
+					style={styles.input}
+					type="checkbox"
+					onChange={() => onChange(todo.id)} />
+				<strong>{index + 1 + '.'}</strong>
+				<span style={styles.span}>{todo.title}</span>
+			</span>
+			<button className="btn" onClick={() => removeTodo(todo.id)}>&times;</button>
+		</li>
 	)
 }
+
+export default TodoItem
