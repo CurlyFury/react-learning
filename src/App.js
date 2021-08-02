bcae5d93bc792e7db7587124caf9c99cc06fe4c2
CurlyFury <nukawanderer@gmail.com>
2021-08-02 13:34:24 +0800

    loader

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import React from 'react';
 import TodoList from "./todo/TodoList";
 import Context from './context';
 import AddTodo from './todo/AddTodo';
+import Loader from './Loader';
 
 
 
 
 function App() {
 
-  const [todoss, setTodos] = React.useState([
-    { id: 1, completed: false, title: 'Buy few eggs' },
-    { id: 2, completed: false, title: 'Buy some butter' },
-    { id: 3, completed: false, title: 'Buy milk' },
-  ])
+  const [todos, setTodos] = React.useState([])
+  const [loadind, setLoading] = React.useState(true)
 
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
       .then(response => response.json())
       .then((todos) => {
-        setTodos(todoss.concat(todos))
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000);
       })
   }, [])
 
   function toggleTodo(id) {
-    setTodos(todoss.map(todo => {
+    setTodos(todos.map(todo => {
       if (todo.id === id) {
         todo.completed = !todo.completed
       }
@@ -35,11 +36,11 @@ function App() {
   }
 
   function removeTodo(id) {
-    setTodos(todoss.filter(todo => todo.id !== id))
+    setTodos(todos.filter(todo => todo.id !== id))
   }
 
   function addTodo(title) {
-    setTodos(todoss.concat([{
+    setTodos(todos.concat([{
       title: title,
       id: Date.now(),
       completed: false,
@@ -53,7 +54,9 @@ function App() {
 
         <AddTodo onCreate={addTodo} />
 
-        {todoss.length ? <TodoList todos={todoss} onToggle={toggleTodo} /> : <p>No todos :( </p>}
+        {loadind && <Loader />}
+
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : loadind ? null : <p>No todos !</p>}
 
       </div>
     </Context.Provider>
---
+++ src/Loader.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
